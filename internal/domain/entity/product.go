f693a3e6c169faaed5a224565e752b40806aa9ec
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-08 02:09:24 -0300

    feat: Adding  specification groups in product entity

--- internal/domain/entity/product.go
+++ internal/domain/entity/product.go
@@ -7,28 +7,31 @@ import (
 )
 
 type Product struct {
-	ID          int64
-	Name        string
-	Description string
-	Price       int64 // in cents R$ 5.012,00 -> 5012
-	Rating      int8  // 0-50 (10 = 1 star, 25 = 2.5 stars, 50 = 5 stars)
+	ID                  int64
+	Name                string
+	Description         string
+	Price               int64 // in cents R$ 5.012,00 -> 5012
+	Rating              int8  // 0-50 (10 = 1 star, 25 = 2.5 stars, 50 = 5 stars)
+	SpecificationGroups []*SpecificationGroup
 }
 
 type ProductProps struct {
-	ID          int64
-	Name        string
-	Description string
-	Price       int64
-	Rating      int8
+	ID                  int64
+	Name                string
+	Description         string
+	Price               int64
+	Rating              int8
+	SpecificationGroups []*SpecificationGroup
 }
 
 func NewProduct(props ProductProps) (*Product, exceptions.EntityException) {
 	product := &Product{
-		ID:          props.ID,
-		Name:        props.Name,
-		Description: props.Description,
-		Price:       props.Price,
-		Rating:      props.Rating,
+		ID:                  props.ID,
+		Name:                props.Name,
+		Description:         props.Description,
+		Price:               props.Price,
+		Rating:              props.Rating,
+		SpecificationGroups: props.SpecificationGroups,
 	}
 
 	err := product.validate()
@@ -57,6 +60,10 @@ func (p *Product) validate() error {
 
 	if len(p.Description) > 2000 {
 		return errors.New("Description cannot be longer than 2000 characters")
+	}
+
+	if p.Rating < 0 || p.Rating > 50 {
+		return errors.New("Rating must be between 0 and 50")
 	}
 
 	return nil
