95cc601c36f4874a66ed4d65f7bc6cd337a8964d
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-08 01:57:35 -0300

    feat: Adding validation to specification group entity

--- internal/domain/entity/spefication_group.go
+++ internal/domain/entity/spefication_group.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"errors"
+	"project/internal/domain/constants"
+	exceptions "project/internal/domain/exception"
+)
+
 type SpecificationGroup struct {
 	ID                int64
 	Name              string
@@ -16,14 +22,44 @@ type SpecificationGroupProps struct {
 	Specifications    []*Specification
 }
 
-func NewSpecificationGroup(props SpecificationGroupProps) SpecificationGroup {
-	return SpecificationGroup{
+func NewSpecificationGroup(props SpecificationGroupProps) (*SpecificationGroup, exceptions.EntityException) {
+	specificationGroup := &SpecificationGroup{
 		ID:                props.ID,
 		Name:              props.Name,
 		Description:       props.Description,
 		TotalSpefications: props.TotalSpefications,
 		Specifications:    props.Specifications,
 	}
+
+	err := specificationGroup.validate()
+
+	if err != nil {
+		return nil, exceptions.Entity(err, exceptions.EntityOpts{
+			Reason: constants.EntityValidationError,
+		})
+	}
+
+	return specificationGroup, nil
+}
+
+func (sg *SpecificationGroup) validate() error {
+	if sg.ID < 0 {
+		return errors.New("ID field cannot be less than 0")
+	}
+
+	if sg.Name == "" {
+		return errors.New("Name cannot be empty")
+	}
+
+	if len(sg.Name) > 255 {
+		return errors.New("Name cannot be longer than 255 characters")
+	}
+
+	if len(sg.Description) > 2000 {
+		return errors.New("Description cannot be longer than 2000 characters")
+	}
+
+	return nil
 }
 
 func (sg *SpecificationGroup) HasSpecifications() bool {
