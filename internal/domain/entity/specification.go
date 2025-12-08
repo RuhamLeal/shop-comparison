68b445f09e34608a439882032a7510e9c2549ab2
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-08 01:57:28 -0300

    feat: Adding validation to specification entity

--- internal/domain/entity/specification.go
+++ internal/domain/entity/specification.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"errors"
+	"project/internal/domain/constants"
+	exceptions "project/internal/domain/exception"
+)
+
 type Specification struct {
 	ID      int64
 	Title   string
@@ -12,10 +18,44 @@ type SpecificationProps struct {
 	Content string
 }
 
-func NewSpecification(props SpecificationProps) Specification {
-	return Specification{
+func NewSpecification(props SpecificationProps) (*Specification, exceptions.EntityException) {
+	specification := &Specification{
 		ID:      props.ID,
 		Title:   props.Title,
 		Content: props.Content,
 	}
+
+	err := specification.validate()
+
+	if err != nil {
+		return nil, exceptions.Entity(err, exceptions.EntityOpts{
+			Reason: constants.EntityValidationError,
+		})
+	}
+
+	return specification, nil
+}
+
+func (s *Specification) validate() error {
+	if s.ID < 0 {
+		return errors.New("ID field cannot be less than 0")
+	}
+
+	if s.Title == "" {
+		return errors.New("Title cannot be empty")
+	}
+
+	if len(s.Title) > 255 {
+		return errors.New("Title cannot be longer than 255 characters")
+	}
+
+	if s.Content == "" {
+		return errors.New("Content cannot be empty")
+	}
+
+	if len(s.Content) > 2000 {
+		return errors.New("Content cannot be longer than 2000 characters")
+	}
+
+	return nil
 }
