0166cbeeef5426f881061d04924a4343f52f45f8
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-09 00:40:28 -0300

    feat: Adding public id field in specification entity

--- internal/domain/entity/specification.go
+++ internal/domain/entity/specification.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"project/internal/domain/constants"
 	exceptions "project/internal/domain/exception"
+	"project/internal/domain/services"
 	. "project/internal/domain/types"
 )
 
 type Specification struct {
 	ID                    int64
+	PublicID              SpecificationPublicID
 	Title                 string
 	EspecificationGroupID SpecificationGroupID
 	Type                  string
@@ -16,20 +18,29 @@ type Specification struct {
 
 type SpecificationProps struct {
 	ID                    int64
+	PublicID              SpecificationPublicID
 	Title                 string
 	EspecificationGroupID SpecificationGroupID
 	Type                  string
 }
 
 func NewSpecification(props SpecificationProps) (*Specification, exceptions.EntityException) {
+	publicID, err := services.GeneratePublicID(props.PublicID)
+	if err != nil {
+		return nil, exceptions.Entity(err, exceptions.EntityOpts{
+			Reason: constants.EntityBussinessError,
+		})
+	}
+
 	specification := &Specification{
 		ID:                    props.ID,
+		PublicID:              publicID,
 		Title:                 props.Title,
 		EspecificationGroupID: props.EspecificationGroupID,
 		Type:                  props.Type,
 	}
 
-	err := specification.validate()
+	err = specification.validate()
 
 	if err != nil {
 		return nil, exceptions.Entity(err, exceptions.EntityOpts{
@@ -47,6 +58,10 @@ func (s *Specification) validate() error {
 
 	if s.EspecificationGroupID <= 0 {
 		return errors.New("EspecificationGroupID field must be greater than 0")
+	}
+
+	if len(s.PublicID) != 8 {
+		return errors.New("PublicID must be exactly 8 characters long")
 	}
 
 	if s.Title == "" {
