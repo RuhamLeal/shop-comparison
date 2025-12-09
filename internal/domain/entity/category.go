48493358502b9e501dfcf95b849a31a7bb13465e
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-08 23:39:38 -0300

    feat: Adding public_id for category entity

--- internal/domain/entity/category.go
+++ internal/domain/entity/category.go
@@ -4,28 +4,40 @@ import (
 	"errors"
 	"project/internal/domain/constants"
 	exceptions "project/internal/domain/exception"
+	"project/internal/domain/services"
+	. "project/internal/domain/types"
 )
 
 type Category struct {
 	ID       int64
+	PublicID CategoryPublicID
 	Name     string
 	Products []*Product
 }
 
 type CategoryProps struct {
 	ID       int64
+	PublicID CategoryPublicID
 	Name     string
 	Products []*Product
 }
 
 func NewCategory(props CategoryProps) (*Category, exceptions.EntityException) {
+	publicID, err := services.GeneratePublicID(props.PublicID)
+	if err != nil {
+		return nil, exceptions.Entity(err, exceptions.EntityOpts{
+			Reason: constants.EntityBussinessError,
+		})
+	}
+
 	category := &Category{
 		ID:       props.ID,
+		PublicID: publicID,
 		Name:     props.Name,
 		Products: props.Products,
 	}
 
-	err := category.validate()
+	err = category.validate()
 
 	if err != nil {
 		return nil, exceptions.Entity(err, exceptions.EntityOpts{
