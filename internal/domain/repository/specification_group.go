4a3bf46642e308da1f09ae54efe87526296aa6a3
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-09 20:55:23 -0300

    feat: Adding new get method in SpecificationGroup repository

--- internal/domain/repository/specification_group.go
+++ internal/domain/repository/specification_group.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"project/internal/domain/entity"
 	. "project/internal/domain/exception"
+	. "project/internal/domain/types"
 )
 
 type SpecificationGroup interface {
 	GetAll() ([]*entity.SpecificationGroup, RepositoryException)
+	GetOneByPublicID(SpecificationGroupPublicID) (*entity.SpecificationGroup, RepositoryException)
 }
