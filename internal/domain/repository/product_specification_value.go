e9586b4e436d63f982614402cd9f0130fa2d7b30
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-09 21:11:56 -0300

    feat: Adding new method in ProductSpecificationValue repository

--- internal/domain/repository/product_specification_value.go
+++ internal/domain/repository/product_specification_value.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"project/internal/domain/entity"
 	. "project/internal/domain/exception"
+	. "project/internal/domain/types"
 )
 
 type ProductSpecificationValue interface {
 	CreateOne(*entity.ProductSpecificationValue) RepositoryException
+	FindManyByProductID(ProductID) ([]*entity.ProductSpecificationValue, RepositoryException)
 }
