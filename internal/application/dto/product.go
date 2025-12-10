cb53a3ba4bd8f0ef7864574f089a75c157776946
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-09 23:15:40 -0300

    feat: Adding GetOneProductWithSpecificationsByPublicId usecase

--- internal/application/dto/product.go
+++ internal/application/dto/product.go
@@ -13,3 +13,32 @@ type GetOneProductByPublicIdOutput struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
 }
+
+type GetOneProductWithSpecificationsByPublicIdInput struct {
+	PublicID types.ProductPublicID `json:"public_id"`
+}
+
+type GetOneProductWithSpecificationsByPublicIdOutput struct {
+	Price                int64                              `json:"price"`
+	Rating               int8                               `json:"rating"`
+	ImageURL             string                             `json:"image_url"`
+	Name                 string                             `json:"name"`
+	Description          string                             `json:"description"`
+	SpecificationsGroups []*ProductSpecificationGroupOutput `json:"specifications_groups"`
+}
+
+type ProductSpecificationGroupOutput struct {
+	PublicID       types.SpecificationGroupPublicID `json:"public_id"`
+	Name           string                           `json:"name"`
+	Description    string                           `json:"description"`
+	Specifications []*ProductSpecificationOutput    `json:"specifications"`
+}
+
+type ProductSpecificationOutput struct {
+	PublicID    types.SpecificationPublicID `json:"public_id"`
+	Title       string                      `json:"name"`
+	Type        types.SpecificationType     `json:"type"`
+	StringValue string                      `json:"string_value"`
+	IntValue    int64                       `json:"int_value"`
+	BoolValue   bool                        `json:"bool_value"`
+}
---
+++ internal/application/usecase/get_one_product_with_specifications_by_public_id.go
@@ -0,0 +1,119 @@
+package usecase
+
+import (
+	"errors"
+	"project/internal/application/dto"
+	"project/internal/application/services"
+	"project/internal/domain/constants"
+	"project/internal/domain/entity"
+	exceptions "project/internal/domain/exception"
+	"project/internal/domain/repository"
+)
+
+type GetOneProductWithSpecificationsByPublicId struct {
+	ProductRepository repository.Product
+	code              string
+}
+
+func NewGetOneProductWithSpecificationsByPublicId(
+	productRepository repository.Product,
+) *GetOneProductWithSpecificationsByPublicId {
+	return &GetOneProductWithSpecificationsByPublicId{
+		code:              "GetOneProductWithSpecificationsByPublicId",
+		ProductRepository: productRepository,
+	}
+}
+
+func (u *GetOneProductWithSpecificationsByPublicId) Execute(input dto.GetOneProductWithSpecificationsByPublicIdInput) (*dto.GetOneProductWithSpecificationsByPublicIdOutput, exceptions.UsecaseException) {
+	productAggregate, repoErr := u.ProductRepository.GetOneByPublicIdWithSpecificationGroups(input.PublicID)
+
+	if repoErr != nil {
+		return nil, exceptions.Usecase(repoErr, exceptions.UsecaseOpts{
+			Code:       u.code,
+			StatusCode: services.GetStatusCodeFromError(repoErr),
+			Message:    "Error getting product with specifications",
+		})
+	}
+
+	output := &dto.GetOneProductWithSpecificationsByPublicIdOutput{
+		Price:                productAggregate.Product.Price,
+		Rating:               productAggregate.Product.Rating,
+		ImageURL:             productAggregate.Product.ImageURL,
+		Name:                 productAggregate.Product.Name,
+		Description:          productAggregate.Product.Description,
+		SpecificationsGroups: []*dto.ProductSpecificationGroupOutput{},
+	}
+
+	if !productAggregate.Product.HasSpecifications() {
+		return output, nil
+	}
+
+	specificationValuesMap := make(map[int64]*entity.ProductSpecificationValue)
+
+	for _, productSpecVal := range productAggregate.Product.SpecificationValues {
+		specificationValuesMap[productSpecVal.SpecificationID] = productSpecVal
+	}
+
+	for _, specificationGroup := range productAggregate.SpecificationsGroups {
+		outputSpecificationsGroup := &dto.ProductSpecificationGroupOutput{
+			PublicID:       specificationGroup.PublicID,
+			Name:           specificationGroup.Name,
+			Description:    specificationGroup.Description,
+			Specifications: []*dto.ProductSpecificationOutput{},
+		}
+
+		for _, specification := range specificationGroup.Specifications {
+			specificationValue, exists := specificationValuesMap[specification.ID]
+
+			if !exists {
+				continue
+			}
+
+			outputSpecification := &dto.ProductSpecificationOutput{
+				PublicID: specification.PublicID,
+				Title:    specification.Title,
+				Type:     specification.Type,
+			}
+
+			switch specification.Type {
+			case constants.SpecificationTypeString:
+				if specificationValue.Value.StringValue == nil {
+					return nil, exceptions.Usecase(errors.New("String value is nil"), exceptions.UsecaseOpts{
+						Code:       u.code,
+						StatusCode: 500,
+						Message:    "Error getting product specification value",
+					})
+				}
+				outputSpecification.StringValue = *specificationValue.Value.StringValue
+			case constants.SpecificationTypeInt:
+				if specificationValue.Value.IntValue == nil {
+					return nil, exceptions.Usecase(errors.New("Int value is nil"), exceptions.UsecaseOpts{
+						Code:       u.code,
+						StatusCode: 500,
+						Message:    "Error getting product specification value",
+					})
+				}
+				outputSpecification.IntValue = *specificationValue.Value.IntValue
+			case constants.SpecificationTypeBool:
+				if specificationValue.Value.BoolValue == nil {
+					return nil, exceptions.Usecase(errors.New("Bool value is nil"), exceptions.UsecaseOpts{
+						Code:       u.code,
+						StatusCode: 500,
+						Message:    "Error getting product specification value",
+					})
+				}
+				outputSpecification.BoolValue = *specificationValue.Value.BoolValue
+			}
+
+			outputSpecificationsGroup.Specifications = append(outputSpecificationsGroup.Specifications, outputSpecification)
+		}
+
+		if len(outputSpecificationsGroup.Specifications) == 0 {
+			continue
+		}
+
+		output.SpecificationsGroups = append(output.SpecificationsGroups, outputSpecificationsGroup)
+	}
+
+	return output, nil
+}
