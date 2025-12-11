ddc2521d4f2811cefdc49e46311e18f874660be9
Ruham Leal <ruhamxlpro@hotmail.com>
2025-12-11 05:08:09 -0300

    feat: improving fiber instance

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,8 +6,23 @@ import (
 	"os/signal"
 	"project/internal/infra/config"
 	"syscall"
+
+	_ "project/docs"
 )
 
+// @title API
+// @version 0.0.1
+// @description API
+// @contact.name Ruham Leal
+// @contact.email ruhamxlpro@hotmail.com
+// @termsOfService
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @host localhost:8085
+// @BasePath /
 func main() {
 	environmentConf := config.NewBaseConfig(".env")
 	mainInstance := config.NewServerInstances(environmentConf)
--- internal/infra/fiber/fiber.go
+++ internal/infra/fiber/fiber.go
@@ -3,10 +3,13 @@ package fiber
 import (
 	"fmt"
 	"project/internal/infra/config/environment"
+	"project/internal/infra/fiber/route"
+	"project/internal/infra/fiber/utils/response"
 	"project/internal/infra/sqlite"
 
 	json "github.com/goccy/go-json"
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/log"
 )
 
 type Fiber struct {
@@ -24,6 +27,24 @@ func NewFiberInstance(
 			AppName:     "Edge DNS API",
 			JSONEncoder: json.Marshal,
 			JSONDecoder: json.Unmarshal,
+			ErrorHandler: func(c fiber.Ctx, err error) error {
+				log.Error(err)
+				if c.Response().StatusCode() == fiber.StatusNotFound {
+					return response.SendNotFound(c, "sorry, endpoint not found", err)
+				}
+
+				return response.SendInternalServerError(c, "sorry, unexpected error occurred", err)
+			},
+		},
+	)
+
+	router := route.NewRouter(app, sqlite)
+
+	router.Load()
+
+	app.Use(
+		func(c fiber.Ctx) error {
+			return response.SendNotFound(c, "Sorry, endpoint not found - unknown route")
 		},
 	)
 
